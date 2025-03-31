e5d9997351282b0bb83d2e5ff6bcfd60e24920ce
twoo0220 <twoo0220@gmail.com>
2025-03-31 23:53:12 +0900

    25.03.31 update

--- 01_textRPG/textRPG/textRPG/CommonType.h
+++ 01_textRPG/textRPG/textRPG/CommonType.h
@@ -2,16 +2,6 @@
 #include <iostream>
 #include <string>
 
-struct Character
-{
-	int hp = 0;
-	int maxHp = 0;
-	int mp = 0;
-	int maxMp = 0;
-	char name[13]{}; // 12 + null character
-	char aa[256]{};
-};
-
 enum class MONSTER_TYPE
 {
 	MONSTER_PLAYER,		// Player
@@ -25,4 +15,23 @@ enum class CHARACTER_TYPE
 	CHARACTER_PLAYER,	// Player
 	CHARACTER_MONSTER,	// Monster
 	CHARACTER_MAX		// Character Type Max
+};
+
+enum class COMMAND_TYPE
+{
+	COMMAND_FIGHT,
+	COMMAND_SPELL,
+	COMMAND_RUN,
+	COMMAND_MAX
+};
+
+struct Character
+{
+	int hp = 0;
+	int maxHp = 0;
+	int mp = 0;
+	int maxMp = 0;
+	char name[13]{}; // 12 + null character
+	char aa[256]{};
+	COMMAND_TYPE command = COMMAND_TYPE::COMMAND_MAX;
 };
--- 01_textRPG/textRPG/textRPG/GameManager.cpp
+++ 01_textRPG/textRPG/textRPG/GameManager.cpp
@@ -26,14 +26,41 @@ void gameManager::Battle()
 	// _getch();
 	std::string orderString;
 	std::cin >> orderString;
+	std::cout << mCharacters[static_cast<int>(CHARACTER_TYPE::CHARACTER_MONSTER)].name << "이(가) 나타났다." << std::endl;
+
+	while (true)
+	{
+		for (int i = 0; i < static_cast<int>(CHARACTER_TYPE::CHARACTER_MAX); ++i)
+		{
+			DrawBattleScreen();
+			switch (mCharacters[i].command)
+			{
+			case COMMAND_TYPE::COMMAND_FIGHT:
+				std::cout << mCharacters[i].name << "이(가) 공격했다." << std::endl;
+				break;
+			case COMMAND_TYPE::COMMAND_SPELL:
+				std::cout << mCharacters[i].name << "이(가) 주문을 외웠다." << std::endl;
+				break;
+			case COMMAND_TYPE::COMMAND_RUN:
+				break;
+			case COMMAND_TYPE::COMMAND_MAX:
+			default:
+				break;
+			}
+		}
+	}
 }
 
 void gameManager::DrawBattleScreen()
 {
+	system("cls");
 	std::cout << mCharacters[static_cast<int>(CHARACTER_TYPE::CHARACTER_PLAYER)].name << std::endl;
 	std::cout << "HP: " << mCharacters[static_cast<int>(CHARACTER_TYPE::CHARACTER_PLAYER)].hp
 		<< " / " << mCharacters[static_cast<int>(CHARACTER_TYPE::CHARACTER_PLAYER)].maxHp
 		<< "  MP: " << mCharacters[static_cast<int>(CHARACTER_TYPE::CHARACTER_PLAYER)].mp
 		<< " / " << mCharacters[static_cast<int>(CHARACTER_TYPE::CHARACTER_PLAYER)].maxMp
 		<< std::endl;
+
+	std::cout << "======================" << std::endl;
+
 }
